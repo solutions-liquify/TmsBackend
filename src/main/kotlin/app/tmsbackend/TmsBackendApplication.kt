a4ac8e3ce9f2f517841b31600ebdc1028a8a8cfa
Aakash Barbhaya <aakashbarbhaya@gmail.com>
2024-12-28 18:58:59 +0530

    returning index.html

--- src/main/kotlin/app/tmsbackend/TmsBackendApplication.kt
+++ src/main/kotlin/app/tmsbackend/TmsBackendApplication.kt
@@ -2,10 +2,22 @@ package app.tmsbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+    
 @SpringBootApplication
 class TmsBackendApplication
 
 fun main(args: Array<String>) {
     runApplication<TmsBackendApplication>(*args)
+}
+
+@CrossOrigin
+@RestController
+class TmsBackendController {
+    @GetMapping("/")
+    fun index(): String {
+        return "Welcome to backend service for TMS"
+    }
 }
