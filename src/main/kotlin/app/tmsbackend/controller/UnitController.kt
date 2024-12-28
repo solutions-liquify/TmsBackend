de9079a5143be1405c49ff4e6df3dadf6c25a7f2
Aakash Barbhaya <aakashbarbhaya@gmail.com>
2024-12-28 14:51:30 +0530

    units api

---
+++ src/main/kotlin/app/tmsbackend/controller/UnitController.kt
@@ -0,0 +1,19 @@
+package app.tmsbackend.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.CrossOrigin
+
+@CrossOrigin
+@RequestMapping("/api/v1/units")
+@RestController
+class UnitController {
+
+    @GetMapping("/list")
+    fun listUnits(): ResponseEntity<List<String>> {
+        val units = listOf("MT", "Kg", "Units", "Ltr")
+        return ResponseEntity.ok(units)
+    }
+}
